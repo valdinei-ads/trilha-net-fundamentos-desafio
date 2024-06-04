d8a0d174128fb6c0885362008e2fa892150a9c08
Valdinei Sacramento de Almeida <valdinei.almeida@consulting-for.edenred.com>
2024-06-03 21:57:15 -0300

    Add ConsoleHelper

---
+++ DesafioFundamentos/Helper/ConsoleHelper.cs
@@ -0,0 +1,31 @@
+﻿namespace DesafioFundamentos.Helper
+{
+    public static class ConsoleHelper
+    {
+        public static void Clear() => Console.Clear();
+
+        public static void LogInformacao(string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine(mensagem);
+        }
+
+        public static void LogSucesso(string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(mensagem);
+        }
+
+        public static void LogAtencao(string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine(mensagem);
+        }
+
+        public static void LogErro(string mensagem)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(mensagem);
+        }
+    }
+}
